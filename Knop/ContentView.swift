3de9d7291a749e4460c238fee9d61a523df4564e
Nick <jacobsnick@live.nl>
2020-10-22 15:23:11 +0200

    Knop met tekst V1.2

--- Knop/ContentView.swift
+++ Knop/ContentView.swift
@@ -9,19 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack{
+            Text(inhoud)
         Button(action:klik, label: {
             Text("""
                 1
                 """).frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        })   }
+        })   }}
     @State var teller=0;
+    @State var inhoud = "gaat goed zo";
+    
      func klik()
     {self.teller+=1;
         print ("hallo")
     
-    if self.teller == 1{print ("hallo1")}
-        if self.teller == 2{print ("hallo2")}
-            if self.teller == 3{print ("goed gedaan")}
+        if self.teller == 1{inhoud = "hallo1";}
+        else if self.teller == 2{inhoud = "hallo2";}
+       else if self.teller == 3{inhoud = "goed gedaan";}
+       else if self.teller == 4{exit(0);}
+        
+        
      }
 
     
