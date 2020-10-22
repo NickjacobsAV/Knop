ebb3664d90df7fb7327d9867e6161d7c01fee068
Nick <jacobsnick@live.nl>
2020-10-22 15:00:09 +0200

    Laatste versie V1.1

--- Knop/ContentView.swift
+++ Knop/ContentView.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("""
-Hello, World!
-             hallo
-""")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
+        Button(action:klik, label: {
+            Text("""
+                1
+                """).frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        })   }
+    @State var teller=0;
+     func klik()
+    {self.teller+=1;
+        print ("hallo")
+    
+    if self.teller == 1{print ("hallo1")}
+        if self.teller == 2{print ("hallo2")}
+            if self.teller == 3{print ("goed gedaan")}
+     }
+
+    
+    
+    
+// Einde
 }
 
 
